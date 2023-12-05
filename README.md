c665955808657b34507b56f239cbaafb218000b5
vk <v.katsuba.dev@gmail.com>
2023-12-05 16:46:57 +0200

    Implement base test for POST/GET/DELETE resourceSpecification

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
-## Tooling
+# Tooling
 * Install [Rust](https://www.rust-lang.org/tools/install)
 * Install [Redis Stack](https://redis.io/docs/install/install-stack/)
 * Install [ODA API Rust SDK: TMF634](https://github.com/oda-components/oda-api-sdk-rust#tmf634)
 * Install [ODA API Rust SDK: TMF639](https://github.com/oda-components/oda-api-sdk-rust#tmf639)
 
-## Build
+# Build
+
 ```bash
 $ cargo build --workspace
 ```
 
-## Document
+# Document
+
 ```bash
 $ cargo doc --workspace
 ```
 
-## Run
+# Run
 
-### Redis
+## Redis
+
 Start the Redis Stack server:
 ```bash
 $ redis-stack-server
@@ -27,14 +30,27 @@ $ redis-cli
 127.0.0.1:6379> FT.CREATE resourceSpecification ON JSON PREFIX 1 resourceSpecification: SCHEMA $.id AS id TAG
 ```
 
-### TMF634
+## TMF634
+
+### Run
+
 ```bash
 $ RUST_LOG=info cargo run --package oda_ri_tmf634 --bin tmf634_server
 $ cargo run --package oda_ri_tmf634 --bin tmf634_client -- --help
 $ RUST_LOG=info cargo run --package oda_ri_tmf634 --bin tmf634_client -- --host localhost ListResourceSpecification
 ```
 
-### TMF639
+### Test
+
+> [!WARNING]
+> Before run tests required run [Redis](README.md#redis) and create index.
+
+```bash
+$ cargo test --package oda_ri_tmf634 --bin tmf634_server -- --nocapture
+```
+
+## TMF639
+
 ```bash
 $ RUST_LOG=info cargo run --package oda_ri_tmf639 --bin tmf639_server
 $ cargo run --package oda_ri_tmf639 --bin tmf639_client -- --help
--- tmf634/server/src/server.rs
+++ tmf634/server/src/server.rs
@@ -847,3 +847,98 @@ impl<C> Api<C> for Server<C> where C: Has<XSpanIdString> + Send + Sync
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use tokio::test;
+    use reqwest;
+    use crate::server::create;
+    use crate::server::models;
+    use serde_json::{Result, Value};
+
+    #[test]
+    async fn resource_specification() {
+        let url = "http://localhost:8080/tmf-api/resourceCatalog/v4/resourceSpecification";
+        // Start the server
+        let server_handle = tokio::spawn(async {
+            let redis = "redis://127.0.0.1";
+            let bind = "0.0.0.0";
+            let port = "8080";
+
+            create(redis, bind, port, false).await;
+        });
+        // Create a client
+        let client = reqwest::Client::new();
+        // Make a GET /resourceSpecification request to the server
+        let get_spec_resp = client
+            .get(url)
+            .send()
+            .await
+            .expect("failed to get response")
+            .text()
+            .await
+            .expect("failed to get payload");
+
+        // Printout raw and decoded JSON
+        println!("GET /resourceSpecification JSON raw response: {:?}", get_spec_resp);
+        println!("GET /resourceSpecification decoded response: {:?}", serde_json::from_str::<Vec<models::ResourceSpecification>>(&get_spec_resp));
+
+        // POST for create new spec
+        let post_spec_resp = client
+            .post(url)
+            .body(r#"{"name": "example"}"#)
+            .send()
+            .await
+            .expect("failed to get response")
+            .text()
+            .await
+            .expect("failed to get payload");
+
+        // Decode POST response
+        let spec_post_entity = serde_json::from_str::<models::ResourceSpecification>(&post_spec_resp);
+
+        println!("POST /resourceSpecification JSON raw response: {:?}", post_spec_resp);
+        println!("POST /resourceSpecification JSON decoded response: {:?}",  spec_post_entity);
+
+        // Fetch ID of new spec from decoded response body of POST request
+        let spec_id: String = spec_post_entity.map(|res| res.id.unwrap_or_default()).unwrap_or_default();
+
+        // GET created spec
+        let get_new_spec_resp = client
+            .get(url.to_owned() + "/" + &spec_id)
+            .send()
+            .await
+            .expect("failed to get response")
+            .text()
+            .await
+            .expect("failed to get payload");
+
+        // Compare spec from GET and POST requests
+        assert_eq!(get_new_spec_resp, post_spec_resp);
+
+        // DELETE spec and get status code - expected 204
+        let del_spec_status = client
+            .delete(url.to_owned() + "/" + &spec_id)
+            .send()
+            .await
+            .unwrap()
+            .status();
+
+        // Compare HTTP status code from DELETE request
+        assert_eq!(del_spec_status, 204);
+
+        // GET deleted spec - expected now 404
+        let get_deleted_spec = client
+            .get(url.to_owned() + "/" + &spec_id)
+            .send()
+            .await
+            .unwrap()
+            .status();
+
+        // Compare GET HTTP status code for deleted spec
+        assert_eq!(get_deleted_spec, 404);
+
+        // Stop the server
+        server_handle.abort();
+    }
+}
